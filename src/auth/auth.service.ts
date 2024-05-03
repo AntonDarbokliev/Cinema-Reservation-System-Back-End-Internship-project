af004d1fce4d0f7067f255d408ec58b03cd962e4
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-03 21:17:03 +0300

    auth login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,15 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { CreateUserDto } from 'src/user/dto';
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from 'src/user/dto/loginUserDto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(dto: CreateUserDto) {
     try {
@@ -22,6 +27,24 @@ export class AuthService {
       });
     } catch (error) {
       throw error;
+    }
+  }
+
+  async login(dto: LoginUserDto) {
+    try {
+      const user = await this.userService.findOne(dto.email);
+
+      const isMatch = await bcrypt.compare(dto.password, user.password);
+
+      if (!isMatch) {
+        throw new ForbiddenException('Invalid credentials');
+      }
+
+      return {
+        access_token: await this.jwtService.signAsync(dto),
+      };
+    } catch (error) {
+      throw new ForbiddenException('Invalid credentials');
     }
   }
 }
