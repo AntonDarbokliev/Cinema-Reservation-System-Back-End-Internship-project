1d8edaafc86116ac0655339e064dbc0f8217c822
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-03 16:43:08 +0300

    user service/schema

---
+++ src/user/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Date, HydratedDocument, now } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true, type: String })
+  firstName: string;
+
+  @Prop({ required: true, type: String })
+  lastName: string;
+
+  @Prop({ required: true, type: Date, default: now() })
+  createdAt: Date;
+
+  @Prop({ required: true, type: String })
+  email: string;
+
+  @Prop({ required: true, type: String })
+  password: string;
+
+  @Prop({ required: true, type: Number, default: 1 })
+  role: number;
+}
+
+export const userSchema = SchemaFactory.createForClass(User);
---
+++ src/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(dto: CreateUserDto) {
+    const createdUser = await this.userModel.create(dto);
+    return createdUser;
+  }
+
+  async findOne(email: string) {
+    return await this.userModel.findOne({ email: email });
+  }
+}
