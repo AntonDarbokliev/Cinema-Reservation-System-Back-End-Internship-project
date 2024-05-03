7f290d746d9f13733e75a94e3a74b5233bbf4be7
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-03 21:17:23 +0300

    use public custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,28 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto';
+import { LoginUserDto } from 'src/user/dto/loginUserDto';
+import { Public } from './decorator';
 
-@Controller('')
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     try {
       return await this.authService.register(createUserDto);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Public()
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    try {
+      return await this.authService.login(loginUserDto);
     } catch (error) {
       console.log(error);
     }
