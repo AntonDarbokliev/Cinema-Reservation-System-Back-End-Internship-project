0fce8c0995913af8ad961e7a2c7418f352fd82e2
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-03 21:18:20 +0300

    Add jwt guard and register jwt for the whole module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtGuard } from './guard';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, { provide: 'APP_GUARD', useClass: JwtGuard }],
   controllers: [AuthController],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: await configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1h' },
+          // global: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AuthModule {}
