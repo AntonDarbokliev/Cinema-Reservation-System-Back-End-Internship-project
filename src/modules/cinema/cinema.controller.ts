c64ee50a31465103ea81085a218f4ad35a979e8e
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-11 18:21:46 +0300

    cinema module/ controller

---
+++ src/modules/cinema/cinema.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateCinemaDto } from './dto';
+import { CinemaService } from './cinema.service';
+import { Public } from '../auth/decorator';
+import { EditCinemaDto } from './dto/editCinemaDto';
+
+@Controller('cinema')
+export class CinemaController {
+  constructor(private cinemaService: CinemaService) {}
+
+  @Post()
+  async addCinema(@Body() createCinemaDto: CreateCinemaDto) {
+    return await this.cinemaService.createCinema(createCinemaDto);
+  }
+
+  @Public()
+  @Get(':id')
+  async getCinema(@Param() params: { id: string }) {
+    return await this.cinemaService.getCinema(params.id);
+  }
+
+  @Patch(':id')
+  async updateCinema(
+    @Param() params: { id: string },
+    @Body() editCinemaDto: EditCinemaDto,
+  ) {
+    return await this.cinemaService.updateCinema(params.id, editCinemaDto);
+  }
+
+  @Delete(':id')
+  async deleteCinema(@Param() params: { id: string }) {
+    return await this.cinemaService.deleteCinema(params.id);
+  }
+}
