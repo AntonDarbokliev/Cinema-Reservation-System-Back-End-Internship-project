fd7a6abaed7bc7f91231c4cc730873dc48062250
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-19 16:13:10 +0300

    movie schema

---
+++ src/modules/movie/movie.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Rating, Genre } from './dto';
+
+@Schema()
+export class Movie {
+  @Prop({ type: String, required: true })
+  rating: Rating;
+
+  @Prop({ type: Array, required: true })
+  genres: Genre[];
+
+  @Prop({ type: String, required: true })
+  length: string;
+
+  @Prop({ type: String, required: true })
+  director: Rating;
+
+  @Prop({ type: String, required: true })
+  description: Rating;
+
+  @Prop({ type: String, required: true })
+  actors: Rating;
+
+  @Prop({ type: String, required: true })
+  language: string;
+
+  @Prop({ type: String })
+  production?: Rating;
+
+  @Prop({ type: String })
+  subtitles?: string;
+}
+
+export const movieSchema = SchemaFactory.createForClass(Movie);
