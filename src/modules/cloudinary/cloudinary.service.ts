26a32602846a989ffc995b3e0f9c6b0a7e71d6fe
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-20 19:58:06 +0300

    adjust images in cloudinary to better suit a poster format

--- src/modules/cloudinary/cloudinary.service.ts
+++ src/modules/cloudinary/cloudinary.service.ts
@@ -8,6 +8,7 @@ export class CloudinaryService {
   uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
     return new Promise<CloudinaryResponse>((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
+        { transformation: { width: 600, height: 900, crop: 'fill' } },
         (error, result) => {
           if (error) return reject(error);
           resolve(result);
