b48fe48ebaf7e6600496ab4ca4fdcd1e99795fa2
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-20 19:58:49 +0300

    link movies to cinemas

--- src/modules/movie/movie.module.ts
+++ src/modules/movie/movie.module.ts
@@ -4,10 +4,12 @@ import { Movie, movieSchema } from './movie.schema';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { Cinema, cinemaSchema } from '../cinema/cinema.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Movie.name, schema: movieSchema }]),
+    MongooseModule.forFeature([{ name: Cinema.name, schema: cinemaSchema }]),
     CloudinaryModule,
   ],
   providers: [MovieService],
