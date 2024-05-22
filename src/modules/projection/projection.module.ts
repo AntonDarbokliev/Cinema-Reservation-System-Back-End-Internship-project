d803829bd3513a373aec792afe290a1d543b5992
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-22 13:37:27 +0300

    projection module

--- src/modules/projection/projection.module.ts
+++ src/modules/projection/projection.module.ts
@@ -4,10 +4,12 @@ import { Movie, movieSchema } from '../movie/movie.schema';
 import { ProjectionController } from './projection.controller';
 import { ProjectionService } from './projection.service';
 import { Projection, projectionSchema } from './projection.schema';
+import { Cinema, cinemaSchema } from '../cinema/cinema.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Movie.name, schema: movieSchema }]),
+    MongooseModule.forFeature([{ name: Cinema.name, schema: cinemaSchema }]),
     MongooseModule.forFeature([
       { name: Projection.name, schema: projectionSchema },
     ]),
