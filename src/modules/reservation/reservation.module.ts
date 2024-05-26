b6aed99c01ed3c9549edcc291c61105fce0df011
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-26 19:50:31 +0300

    reservation module

---
+++ src/modules/reservation/reservation.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ReservationService } from './reservation.service';
+import { ReservationController } from './reservation.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Reservation, reservationSchema } from './reservation.schema';
+import { Seat, seatSchema } from '../hall/hall.schema';
+import { Projection, projectionSchema } from '../projection/projection.schema';
+
+@Module({
+  providers: [ReservationService],
+  controllers: [ReservationController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Reservation.name, schema: reservationSchema },
+    ]),
+    MongooseModule.forFeature([{ name: Seat.name, schema: seatSchema }]),
+    MongooseModule.forFeature([
+      { name: Projection.name, schema: projectionSchema },
+    ]),
+  ],
+})
+export class ReservationModule {}
