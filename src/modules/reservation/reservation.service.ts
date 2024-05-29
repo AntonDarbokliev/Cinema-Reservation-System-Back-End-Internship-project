97969682aaa8891c4f20f1bf78dc6025eb293c9f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-29 18:21:46 +0300

    create reservation service

--- src/modules/reservation/reservation.service.ts
+++ src/modules/reservation/reservation.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Reservation } from './reservation.schema';
 import { getCurrentDateTime } from './utils/getCurrentDateTime';
 import { CreateReservationDto } from './dto/createReservationDto';
+import { ReservationStatus } from './dto/reservationStatus';
 
 @Injectable()
 export class ReservationService {
   constructor(
     @InjectModel('Reservation') private reservationModel: Model<Reservation>,
   ) {}
+
+  async getProjectionReservations(projectionId: string) {
+    return await this.reservationModel.find({ projection: projectionId });
+  }
 
   async getSingleReservation(reservationId: string) {
     return await this.reservationModel
@@ -19,7 +24,27 @@ export class ReservationService {
   }
 
   async createReservation(dto: CreateReservationDto) {
+    const reservations = await this.getProjectionReservations(dto.projection);
+    if (
+      reservations.some(
+        (reservation) =>
+          (reservation.seatRow === dto.seatRow &&
+            reservation.seatNumber === dto.seatNumber) ||
+          reservation.seat._id === dto.seat,
+      )
+    ) {
+      throw new BadRequestException('Seat already reserved');
+    }
     return await this.reservationModel.create(dto);
+  }
+
+  async updateReservationStatus(
+    reservationId: string,
+    newStatus: ReservationStatus,
+  ) {
+    return await this.reservationModel.findByIdAndUpdate(reservationId, {
+      status: newStatus,
+    });
   }
 
   async cancelReservation(reservationId: string) {
@@ -33,7 +58,7 @@ export class ReservationService {
     ) {
       return await this.reservationModel.findByIdAndDelete(reservationId);
     } else {
-      throw new Error(
+      throw new BadRequestException(
         'Cannot cancel reservation later than one day in advance',
       );
     }
