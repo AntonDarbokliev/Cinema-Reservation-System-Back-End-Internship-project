53ef51379b7a71cac2ffa342a17e6fe2294a6543
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-31 15:49:25 +0300

    get single movie/ edit movie routes

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  Param,
+  Patch,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -10,17 +12,23 @@ import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/createMovieDto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { EditMovieDto } from './dto/editMovieDto';
 
 @Controller('movies')
 export class MovieController {
   constructor(
     private movieService: MovieService,
-    private cloudinarySerive: CloudinaryService,
+    private cloudinaryService: CloudinaryService,
   ) {}
 
   @Get()
   async getMovies() {
     return await this.movieService.getMovies();
+  }
+
+  @Get(':movieId')
+  async getMovie(@Param('movieId') movieId: string) {
+    return await this.movieService.getMovie(movieId);
   }
 
   @Post()
@@ -29,8 +37,24 @@ export class MovieController {
     @Body() movieDto: CreateMovieDto,
     @UploadedFile() poster: Express.Multer.File,
   ) {
-    const imageUrl = (await this.cloudinarySerive.uploadFile(poster, true)).url;
+    const imageUrl = (await this.cloudinaryService.uploadFile(poster, true))
+      .url;
 
     return await this.movieService.createMovie(movieDto, imageUrl);
+  }
+
+  @Patch(':movieId')
+  @UseInterceptors(FileInterceptor('poster'))
+  async editMovie(
+    @Body() movieDto: EditMovieDto,
+    @UploadedFile() poster: Express.Multer.File,
+    @Param('movieId') movieId: string,
+  ) {
+    const movie = await this.movieService.getMovie(movieId);
+    const publicImageId = this.cloudinaryService.getPublicId(movie.poster);
+    await this.cloudinaryService.deleteImage(publicImageId);
+    const imageUrl = (await this.cloudinaryService.uploadFile(poster, true))
+      .url;
+    return await this.movieService.editMovie(movieDto, imageUrl, movieId);
   }
 }
