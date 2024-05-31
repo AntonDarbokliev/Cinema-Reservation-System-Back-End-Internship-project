33a5bce7a0c592993eeefb202ddc7db8eeafa45c
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-05-31 15:49:55 +0300

    edit movie dto

---
+++ src/modules/movie/dto/editMovieDto.ts
@@ -0,0 +1,48 @@
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Rating, Genre } from './index';
+
+export class EditMovieDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  rating: Rating;
+
+  @IsArray()
+  @IsNotEmpty()
+  genres: Genre[];
+
+  @IsNotEmpty()
+  @IsString()
+  length: string;
+
+  @IsNotEmpty()
+  @IsString()
+  director: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  actors: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  language: string;
+
+  @IsString()
+  @IsOptional()
+  production: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  subtitles: string[];
+
+  @IsOptional()
+  @IsString()
+  cinemaId: string;
+}
