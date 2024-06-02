fe5b1052bb53ca27930c7b9cc65a88dfb852fc1b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-03 00:38:03 +0300

    Emit connect/disconnect events when a user interacts with the websocket

--- src/modules/reservation/reservation.gateway.ts
+++ src/modules/reservation/reservation.gateway.ts
@@ -1,16 +1,42 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { CreateSelectSeatDto } from './dto/createSelectSeatDto';
 import { Server } from 'socket.io';
+import { Socket } from 'dgram';
 
-@WebSocketGateway()
-export class ReservationGateway {
+enum SocketEvent {
+  CONNECT = 'establishConnection',
+  DISCONNECT = 'disconnect',
+  SET_SEAT = 'setSeat',
+  ERROR = 'err',
+}
+
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:5173'],
+  },
+})
+export class ReservationGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('Client connected');
+    client.emit(SocketEvent.CONNECT); // Emit to the specific connected client
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client);
+    this.server.emit(SocketEvent.DISCONNECT);
+  }
 
   @SubscribeMessage('selectSeat')
   selectSeat(@MessageBody() body: CreateSelectSeatDto) {
