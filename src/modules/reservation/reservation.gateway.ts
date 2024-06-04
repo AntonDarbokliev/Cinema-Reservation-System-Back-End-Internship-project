502c239bdbd3088e9159e22a45da9dadd75187c2
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-04 15:16:38 +0300

    correctly emit events to the client

--- src/modules/reservation/reservation.gateway.ts
+++ src/modules/reservation/reservation.gateway.ts
@@ -6,20 +6,19 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { CreateSelectSeatDto } from './dto/createSelectSeatDto';
 import { Server } from 'socket.io';
-import { Socket } from 'dgram';
 
 enum SocketEvent {
   CONNECT = 'establishConnection',
-  DISCONNECT = 'disconnect',
+  DISCONNECT = 'disconnectSocket',
   SET_SEAT = 'setSeat',
+  UNSET_SEAT = 'unsetSeat',
   ERROR = 'err',
 }
 
 @WebSocketGateway({
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
   },
 })
 export class ReservationGateway
@@ -28,19 +27,26 @@ export class ReservationGateway
   @WebSocketServer()
   server: Server;
 
-  handleConnection(client: Socket) {
-    console.log('Client connected');
-    client.emit(SocketEvent.CONNECT); // Emit to the specific connected client
+  handleConnection(client: any) {
+    console.log('Client connected', client.id);
+    client.emit(SocketEvent.CONNECT);
   }
 
-  handleDisconnect(client: Socket) {
-    console.log('Client disconnected:', client);
-    this.server.emit(SocketEvent.DISCONNECT);
+  handleDisconnect(client: any) {
+    console.log('Client disconnected', client.id);
+
+    client.emit(SocketEvent.DISCONNECT);
   }
 
-  @SubscribeMessage('selectSeat')
-  selectSeat(@MessageBody() body: CreateSelectSeatDto) {
+  @SubscribeMessage(SocketEvent.SET_SEAT)
+  selectSeat(@MessageBody() body: any) {
     console.log(body);
-    this.server.emit('selectSeat', body);
+    this.server.emit(SocketEvent.SET_SEAT, body);
+  }
+
+  @SubscribeMessage(SocketEvent.UNSET_SEAT)
+  unselectSeat(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit(SocketEvent.UNSET_SEAT, body);
   }
 }
