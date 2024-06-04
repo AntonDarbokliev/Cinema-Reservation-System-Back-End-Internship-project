0d806b9667e8d260cfdb851f521403de1d9a566c
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-06-04 15:16:58 +0300

    enable cors for second dev enviorment for webosocket testing

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+  });
   await app.listen(3000);
 }
 bootstrap();
